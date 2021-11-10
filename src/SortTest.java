50d924f9e6e391625c54417fc2ce48a3ae6dcd68
SaxProfanityBandit <rsahlgren@gmail.com>
2021-11-10 13:18:15 +0100

    Had to rewrite heapsort for negative numbers.

--- src/SortTest.java
+++ src/SortTest.java
@@ -1,18 +1,30 @@
 public class SortTest {
     public static void main(String[] args) {
-      //Testing Class.
-      int[] arr1 = {-10, 3, 4, 10, -6, 9, 11, -13};
-      int[] arr2 = {0, 9, 153, 88, -63, 111, 2, 34};
-      int[] arr3 = {1000, 0, 48, -1, 546, 89, 534, -1000};
-      int[] arr4 = {8, 7, 6, 5, 4, 3, 2, 1};
-      int[] arr5 = {-8, -7, -6, -5, -4, -3, -2, -1};
+        //Testing Class.
+        int[] arr1 = {-10, 3, 4, 10, -6, 9, 11, -13};
+        int[] arr2 = {0, 9, 153, 88, -63, 111, 2, 34};
+        int[] arr3 = {1000, 0, 48, -1, 546, 89, 534, -1000};
+        int[] arr4 = {8, 7, 6, 5, 4, 3, 2, 1};
+        int[] arr5 = {-8, -7, -6, -5, -4, -3, -2, -1};
     
+        printArray(arr1);
+        //int[] sorted = new int[arr1.length];
+        Sorter.heapSort(arr1);
+        printArray(arr1);
+        //sorted = Sorter.heapSort(arr2);
+        //printArray(sorted);
+        //sorted = Sorter.heapSort(arr3);
+        //printArray(sorted);
+        //sorted = Sorter.heapSort(arr4);
+        //printArray(sorted);
+        //sorted = Sorter.heapSort(arr5);
+        //printArray(sorted);
     }
 
     private static void printArray(int[] arr) {
         System.out.println("");
-        for (int n : arr) {
-            System.out.println(n);
+        for (int n = 0; n < arr.length; n++) {
+            System.out.println(arr[n]);
         }
     }
 }
--- src/Sorter.java
+++ src/Sorter.java
@@ -1,25 +1,25 @@
 public class Sorter {
 
     //As using the built in sort in Java is a bit too easy, writing heapsort in java instead.
-    public static int[] heapSort(int[] unsorted) {
-        int[] sorted = new int[unsorted.length];
+    //Had to rewrite it with a more complex one than originally planned due to not handling negative values correctly.
+    public static int[] heapSort(int[] unsorted)
+    {
+        int[] arr = unsorted;
+        int n = arr.length-1;
+        buildheap(arr, n);
 
-        //Create the heap tree and rearrange the array.
-        for (int i = (unsorted.length / 2 - 1); i > 0; i--) {
-            heapify(unsorted, unsorted.length, i);
+        for(int i = n; i > 0; i--) {
+            exchange(arr, 0, i);
+            n = n - 1;
+            heapify(arr, n, 0);
         }
+        return arr;
+    }
 
-        //Extract elements from the heap.
-        for (int i = (unsorted.length - 1); i > 0; i--) {
-            //Move to end.
-            int temp = unsorted[0];
-            unsorted[0] = unsorted[i];
-            unsorted[i] = temp;
-
-            heapify(unsorted, i, 0);
+    private static void buildheap(int [] arr, int n) {
+        for(int i = n / 2; i >= 0; i--) {
+            heapify(arr, n, i);
         }
-        sorted = unsorted;
-        return sorted;
     }
 
     private static void heapify(int arr[], int n, int i) {
@@ -30,23 +30,29 @@ public class Sorter {
         int right = 2 * i + 2;
 
         //Is left larger than root?
-        if (left < n && arr[left] > arr[largest]) {
+        if (left <= n && arr[left] > arr[largest]) {
             largest = left;
+        }
+        else {
+            largest = i;
         }
 
         //Is right larger than root/largest so far?
-        if (right < n && arr[right] > arr[largest]) {
+        if (right <= n && arr[right] > arr[largest]) {
             largest = right;
         }
         
         //Is largest not root of the heap/tree? 
         if (largest != i) {
-            int swap = arr[i];
-            arr[i] = arr[largest];
-            arr[largest] = swap;
-
+            exchange(arr, i, largest);
             //Recursive heapify.
             heapify(arr, n, largest);
         }
+    }
+
+    private static void exchange(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp; 
     }
 }
