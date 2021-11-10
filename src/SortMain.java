13924de2c0a93775e0cd146d46cbe28f8b173595
SaxProfanityBandit <rsahlgren@gmail.com>
2021-11-10 12:39:28 +0100

    First iteration of SortMain done, now SortTest

--- src/SortMain.java
+++ src/SortMain.java
@@ -1,9 +1,22 @@
+import javax.swing.JOptionPane;
 public class SortMain {
     public static void main(String[] args) {
-        int arr[] = { 19, 9, 1, 8, 4, 3 };
-        arr = Sorter.heapSort(arr);
+        //Input from String into Int, using Swing.
+        String numbersString = JOptionPane.showInputDialog(null, "Enter the numbers you want sorted, seperate them with commas.");
+        
+        //Splitting the input string and parsing into int
+        String[] unsortedString = numbersString.split(",");
+        int[] unsorted = new int[unsortedString.length];
+        for (int i = 0; i < unsortedString.length; i++) {
+            unsorted[i] = Integer.parseInt(unsortedString[i]);
+        }
 
-        for(int n : arr) {
+        //Sorting the input numbers and storing in a new array.
+        int[] sorted = new int[unsorted.length];
+        sorted = Sorter.heapSort(unsorted);
+
+        //Print out the sorted array.
+        for(int n : sorted) {
             System.out.println(n);
         }
     }
---
+++ src/SortTest.java
@@ -0,0 +1,18 @@
+public class SortTest {
+    public static void main(String[] args) {
+      //Testing Class.
+      int[] arr1 = {-10, 3, 4, 10, -6, 9, 11, -13};
+      int[] arr2 = {0, 9, 153, 88, -63, 111, 2, 34};
+      int[] arr3 = {1000, 0, 48, -1, 546, 89, 534, -1000};
+      int[] arr4 = {8, 7, 6, 5, 4, 3, 2, 1};
+      int[] arr5 = {-8, -7, -6, -5, -4, -3, -2, -1};
+    
+    }
+
+    private static void printArray(int[] arr) {
+        System.out.println("");
+        for (int n : arr) {
+            System.out.println(n);
+        }
+    }
+}
