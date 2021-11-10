4e92c14d1779d03fc15ab626c25516b17596dba5
SaxProfanityBandit <rsahlgren@gmail.com>
2021-11-10 13:40:08 +0100

    Added functionality for commandline arguments

--- src/SortTest.java
+++ src/SortTest.java
@@ -1,30 +1,48 @@
 public class SortTest {
     public static void main(String[] args) {
-        //Testing Class.
-        int[] arr1 = {-10, 3, 4, 10, -6, 9, 11, -13};
-        int[] arr2 = {0, 9, 153, 88, -63, 111, 2, 34};
-        int[] arr3 = {1000, 0, 48, -1, 546, 89, 534, -1000};
-        int[] arr4 = {8, 7, 6, 5, 4, 3, 2, 1};
-        int[] arr5 = {-8, -7, -6, -5, -4, -3, -2, -1};
-    
-        printArray(arr1);
-        //int[] sorted = new int[arr1.length];
-        Sorter.heapSort(arr1);
-        printArray(arr1);
-        //sorted = Sorter.heapSort(arr2);
-        //printArray(sorted);
-        //sorted = Sorter.heapSort(arr3);
-        //printArray(sorted);
-        //sorted = Sorter.heapSort(arr4);
-        //printArray(sorted);
-        //sorted = Sorter.heapSort(arr5);
-        //printArray(sorted);
+        //Testing Class. Defaults to else behavior if no args entered.
+        if (args.length > 0) {
+            int[] unsorted = new int[args.length];
+            System.out.println("");
+            for(int i = 0; i < args.length; i++) {
+                unsorted[i] = Integer.parseInt(args[i]);
+            }
+            int[] sorted = Sorter.heapSort(unsorted);
+            printArray(sorted);
+        }
+        else {
+            int[] arr1 = {-10, 3, 4, 10, -6, 9, 11, -13};
+            int[] arr2 = {0, 9, 153, 88, -63, 111, 2, 34};
+            int[] arr3 = {1000, 0, 48, -1, 546, 89, 534, -1000};
+            int[] arr4 = {8, 7, 6, 5, 4, 3, 2, 1};
+            int[] arr5 = {-8, -7, -6, -5, -4, -3, -2, -1};
+
+            //Sorting and printing the test arrays above.
+            int[] sorted = new int[arr1.length];
+            sorted = Sorter.heapSort(arr1);
+            printArray(sorted);
+            sorted = Sorter.heapSort(arr2);
+            printArray(sorted);
+            sorted = Sorter.heapSort(arr3);
+            printArray(sorted);
+            sorted = Sorter.heapSort(arr4);
+            printArray(sorted);
+            sorted = Sorter.heapSort(arr5);
+            printArray(sorted);
+        }
     }
 
     private static void printArray(int[] arr) {
+        //Same line print method with values seperated with commas, but \n between arrays/calls of this method.
         System.out.println("");
         for (int n = 0; n < arr.length; n++) {
-            System.out.println(arr[n]);
+            if (n == arr.length-1) {
+                System.out.print(arr[n]);
+            }
+            else {
+                System.out.print(arr[n] + ", ");
+            }
+            
         }
     }
 }
